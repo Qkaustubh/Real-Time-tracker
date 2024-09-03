2168acef621fb839e84860e072874a8cdc1abd47
QwareK <qwarek@kali.QwareK>
2024-09-03 15:11:43 -0400

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+const socket = io();
+console.log("heyyyyy");
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            // Emit location data to the server
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error("Geolocation error:", error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
+
+const map = L.map('map').setView([0, 0], 10);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+    map.setView([latitude, longitude], 13);
+});
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
